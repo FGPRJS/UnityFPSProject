341dcda64888d282d51bbe8f3b25740ae55669b8
FGCRJS <01minted.jowly@icloud.com>
2022-04-15 12:17:42 +0900

    HealthBar with HPText

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,9 +8,12 @@ public class HealthBar : MonoBehaviour
 {
     public MechaBase Target;
 
+    public Text HPText;
+
     public Slider Fill, FillRed;
 
-    float lerp = 0f, duration = 10f;
+    float lerp = 0f;
+    public float duration = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +23,19 @@ public class HealthBar : MonoBehaviour
 
         Fill.value = Target.data.HP;
         FillRed.value = Target.data.HP;
+
+        HPText.text = BuildHPText(Target.data.HP, Target.data.MaxHP);
+    }
+
+    string BuildHPText(long HP, long MaxHP)
+    {
+        StringBuilder builder = new StringBuilder();
+
+        builder.Append(HP);
+        builder.Append(" / ");
+        builder.Append(MaxHP);
+
+        return builder.ToString();
     }
 
     // Update is called once per frame
@@ -31,5 +48,7 @@ public class HealthBar : MonoBehaviour
             lerp += Time.deltaTime / duration;
             FillRed.value = (int)Mathf.Lerp(FillRed.value, Fill.value, lerp);
         }
+
+        HPText.text = BuildHPText((int)FillRed.value, Target.data.MaxHP);
     }
 }
